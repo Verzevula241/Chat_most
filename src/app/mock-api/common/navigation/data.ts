4419bdb1b40c98d7bcff18c8b29606f5413fa94e
MIKE ZAPATRIN <misha.zapatrin@yandex.ru>
2021-09-06 16:58:47 +0300

    first commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,67 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'chat',
+        title: 'Чат',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/chat'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'chat',
+        title: 'Чат',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/chat'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'chat',
+        title: 'Чат',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/chat'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'chat',
+        title: 'Чат',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/chat'
+    }
+];
